d87fa1c40ac3734263ba687fd4aa712f9d8cca21
Pdbjork <86097492+Pdbjork@users.noreply.github.com>
2023-06-29 11:50:41 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+class LearningGoal:
+    def __init__(self, goal):
+        self.goal = goal
+
+class User:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.learning_goals = []
+
+    def set_learning_goal(self, goal):
+        # Create a new learning goal object
+        new_goal = LearningGoal(goal)
+        # Add the learning goal to the user's list of learning goals
+        self.learning_goals.append(new
